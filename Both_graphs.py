7f264dd4c0d05fd41f06728148202540d5850411
daduranka <116219580+daduranka@users.noreply.github.com>
2022-12-11 19:59:06 -0700

    changing direction on UI interactions

--- Both_graphs.py
+++ Both_graphs.py
@@ -7,6 +7,7 @@ from bokeh.models.tools import HoverTool
 from bokeh.palettes import Category10_10
 import itertools
 from bokeh.models.widgets import PreText, Div
+import datetime
 
 ## Bar graph
 df = pd.read_json(f'{os.getcwd()}/project_data_json/covidinfo2022-12-10.json',orient ='index')
@@ -16,23 +17,6 @@ output_file("index.html")
 source = ColumnDataSource(dict(newdf))
 
 countries = source.data['Country,Other'].tolist()
-
-##########################################################################################################################################
-# Creating the Slider 
-#range_slider = RangeSlider(title=" Adjust X-Axis range",start=0,end=13,step=1,value=(plot.x_range.start, plot.x_range.end))
-#range_slider.js_link("value", plot.x_range, "start", attr_selector=0)
-#range_slider.js_link("value", plot.x_range, "end", attr_selector=1)
-# displaying the output
-#layout = layout([range_slider], [plot])
-
-# Creating the Slider 
-#range_slider = RangeSlider(title=" Adjust X-Axis range",start=0,end=13,step=1,value=(plot.x_range.start, plot.x_range.end))
-#range_slider.js_link("value", plot.x_range, "start", attr_selector=0)
-#range_slider.js_link("value", plot.x_range, "end", attr_selector=1)
-# displaying the output
-#layout = layout([range_slider], [plot])
-#show(layout)
-###########################################################################################################################################
 
 p = figure(title = 'Total Deaths from Covid per country, use zoom, pan and hover tools to better explore the data',
            x_axis_label = 'Number of Deaths', y_axis_label = 'Country', y_range = countries, plot_width = 1000, 
@@ -48,31 +32,28 @@ hover.tooltips=[
     ('Total Cases', '@TotalCases'),
     ('Total Deaths', '@TotalDeaths')
 ]
-
-## Line Graph 
-dates = ['2022-11-30','2022-12-01','2022-12-03','2022-12-08','2022-12-09'] # test sample of dates should change using DateRangeSlider 
-countries =['USA','China','Israel','Mexico','UK']# sample selection of countries, shuld be able to select countries using MultiChoice Widget
+last_week_dates = []
+## Line Graph with new death trends for the four most populous countries in the world for past week
+for i in range(0,7):
+    date = datetime.date.today() - datetime.timedelta(days=i)
+    last_week_dates.insert(0, str(date))
+    
+dates = last_week_dates # test sample of dates should change using DateRangeSlider 
+countries =['China','India','USA','Indonesia']# four most populous countries
 condition = 'NewDeaths' # condition should be able to change using Dropdown widget 
 data ={}
 
-#################################################################################################################################################
-#OPTIONS = [("1", "foo"), ("2", "bar"), ("3", "baz"), ("4", "quux")]
-
-#multi_select = MultiSelect(value=["1", "2"], options=OPTIONS)
-#multi_select.js_on_change("value", CustomJS(code="""
-#    console.log('multi_select: value=' + this.value, this.toString())
-#"""))
-#################################################################################################################################################
-
 for date in dates: # This double loop organizes the dat and creates the data frame used as the source in bokeh plotting
-    tempdf= pd.read_json(f'{os.getcwd()}/project_data_json/covidinfo{date}.json', orient ='index')
-    tempdf.set_index('Country,Other')
-    tempdf = tempdf.replace(',','', regex=True)
-    countrydata = {}
-    for country in countries: 
-        countrydata[country] = tempdf.at[country,condition].replace('+','')
-    data[date] = countrydata
-    
+    try:
+        tempdf= pd.read_json(f'{os.getcwd()}/project_data_json/covidinfo{date}.json', orient ='index')
+        tempdf.set_index('Country,Other')
+        tempdf = tempdf.replace(',','', regex=True)
+        countrydata = {}
+        for country in countries: 
+            countrydata[country] = tempdf.at[country,condition].replace('+','')
+        data[date] = countrydata
+    except:
+        print('We did not save data for that date')
 # These next few lines create and reorganize the dataframe 
 final = pd.DataFrame(data)
 final = final.T
